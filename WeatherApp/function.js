ab5bc882adb0bbea8e5a54613c30e72e49d5c5ff
Yashpal Bhati <yashpalbhati699@gmail.com>
2026-04-05 12:53:51 +0530

    Add files via upload

---
+++ WeatherApp/function.js
@@ -0,0 +1,10 @@
+// function text() {
+//   let ans = document.getElementById("search-box").value;
+//   document.getElementById("location").innerText = ans;
+// }
+function text(){
+}
+function SearchedText() {
+  let ans = document.getElementById("search-box").value;
+  document.getElementById("location").innerText = ans;
+}
